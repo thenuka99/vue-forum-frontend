ef9c1fda81aa1075503274e832a38f89dede4f42
thenuka99 <janith.h@creativesoftware.com>
2023-01-17 15:15:07 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
+<navMenu/>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/signup">signup</router-link> |
+    <router-link to="/signin">signin</router-link>
   </nav>
   <router-view/>
 </template>
+
+
+
+<script>
+import navMenu from './components/NavMenu.vue';
+export default {
+components:{
+  navMenu,
+}
+}
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// fontawesome
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+// main css file
+import"./assets/tailwind.css";
+
 createApp(App).use(store).use(router).mount('#app')
