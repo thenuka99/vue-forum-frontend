53333e31307bb9a46428ac4b60e337924a07eeb4
thenuka99 <janith.h@creativesoftware.com>
2023-01-24 13:30:12 +0530

    home #2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,20 +6,42 @@
     laboriosam. Molestiae, similique.
   </div>
   <div class="div flex flex-row-reverse ml-4">
-    <button class="btn p-3 mx-5 mt-4 bg-green-500 rounded-xl"><i class="fa fa-add"></i> add new forum</button>
+      <button class="btn p-3 mx-5 mt-4 bg-green-500 rounded-xl" @click="addforum">
+        <router-link to="/addforum"><i class="fa fa-add"></i> add new forum</router-link>
+      </button>
   </div>
   <div>
+    <!-- content -->
     <div class="div px-5 py-4">
       <div class="grid grid-cols-3 gap-1">
-        <div class="div bg-slate-300 rounded-lg">
+        <!-- forum categories -->
+        <div class="div bg-slate-100 rounded-lg">
           <div class="heading text-left py-4 pl-4 font-semibold">Forums</div>
-          <!-- <pre>{{categoryState}}</pre> -->
-          <div class="div px-3 pb-2 " v-for="category in categoryState.categories" v-bind:key="category.id">
-            <div class="div text-left px-1 font-normal border-b-2 ">
-              {{ category.name }}</div>
-          </div> 
+          <ul class="px-3 pb-2 divide-y divide-slate-300">
+            <li v-for="category in categoryState.categories" v-bind:key="category._id"
+              class="text-left px-1 font-normal  ">
+              {{ category.name }}
+            </li>
+          </ul>
         </div>
-        <div class="col-span-2 bg-green-700">07</div>
+        <!-- forums -->
+        <div class="col-span-2 bg-slate-50 rounded-lg">
+          <ul role="list" class="p-6 divide-y divide-slate-200">
+            <li v-for="post in postState.posts" v-bind:key="post._id" class="flex py-4 first:pt-0 last:pb-0">
+              <div class="ml-3 w-full overflow-hidden ">
+                <p class="text-sm font-medium text-slate-900 text-left">{{ post.title }}</p>
+                <p class="text-sm text-slate-500 truncate text-left"><i class="fa fa-heart text-red-600"></i> {{
+                  post.votes.length
+                }} | <i class="fa fa-comment"></i> {{ post.comments.length }}</p>
+              </div>
+              <div class="w-full">
+                <p>{{ post.updatedOn }}</p>
+                <img class="h-10 w-10 rounded-full float-right"
+                  src="https://cdn-icons-png.flaticon.com/512/4140/4140048.png" alt="" />
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -30,12 +52,24 @@ import { mapGetters } from "vuex";
 import "../assets/tailwind.css";
 export default {
   name: "HomeView",
+  data() {
+    return {
+      categoryId: '',
+      pageNo: '',
+    }
+  },
   computed: mapGetters({
     categoryState: "getCategoryState",
+    postState: "getPostState",
   }),
   created: function () {
     this.$store.dispatch("getAllCategories");
+    this.$store.dispatch("getAllPosts");
   },
+  methods:{
+    addforum(){
+      return this.$router.push("/addforum");
+    }
+  }
 };
 </script>
-
