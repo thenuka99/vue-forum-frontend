22e32b3ce55206397d569ccbe18524b333f048e7
thenuka99 <janith.h@creativesoftware.com>
2023-02-01 14:07:18 +0530

    category list component

---
+++ src/components/CategoryListComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="heading text-left py-4 pl-4 font-semibold">Forums</div>
+           <ul class="px-3 pb-2 divide-y divide-slate-300" >
+             <li v-for="category in categoryState.categories" v-bind:key="category._id "
+               class="text-left px-1 font-normal " @click="resetPage() ">
+               <router-link :to="`/category/${category._id}`">
+               <button>{{ category.name }}</button>
+             </router-link>
+             </li>
+           </ul>
+ </template>
+ 
+ <script>
+ import { mapGetters } from 'vuex';
+
+ export default {
+     computed: mapGetters({
+     categoryState: "getCategoryState",
+   }),
+   created:function(){
+     this.$store.dispatch("getAllCategories");
+   },
+   methods:{
+     resetPage() {
+       this.$store.dispatch("resetPage")
+     },
+   }
+ }
+ </script>
+ 
+ <style>
+ 
+ </style>
