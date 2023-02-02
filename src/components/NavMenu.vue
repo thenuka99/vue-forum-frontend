1b42c8570d44cea2bf7a10bf22eb3e4af2828f13
thenuka99 <janith.h@creativesoftware.com>
2023-02-02 11:06:13 +0530

    nav #3

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,161 +1,102 @@
 <template>
-  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
-          <DisclosureButton
-            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-          >
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </DisclosureButton>
-        </div>
-        <div
-          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
-        >
-          <div class="flex flex-shrink-0 items-center">
-            <img
-              class="block h-8 w-auto lg:hidden"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company"
-            />
-            <img
-              class="hidden h-8 w-auto lg:block"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company"
-            />
-          </div>
-          <div class="hidden sm:ml-6 sm:block">
-            <div class="flex space-x-4">
-              <router-link v-for="item in navigation"
-                :key="item.name" :to="item.href" :class="[
-                  item.active
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'px-3 py-2 rounded-md text-sm font-medium',
-                ]">{{ item.name }}</router-link>
-            </div>
-          </div>
-        </div>
-        <div
-          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-        >
-          <button
-            type="button"
-            class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          >
-            <span class="sr-only">View notifications</span>
-            <BellIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
+  <nav class="bg-white shadow-lg">
+    <div class="max-w-6xl mx-auto px-4">
+      <div class="flex justify-between">
+        <div class="flex space-x-7">
 
-          <!-- Profile dropdown -->
-          <Menu as="div" class="relative ml-3">
-            <div>
-              <MenuButton
-                class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-                <span class="sr-only">Open user menu</span>
-                <img v-if="userState.user"
-                  class="h-8 w-8 rounded-full"
-                  :src="userState.user.imageurl"
-                  alt=""
-                />
-              </MenuButton>
-            </div>
-            <transition
-              enter-active-class="transition ease-out duration-100"
-              enter-from-class="transform opacity-0 scale-95"
-              enter-to-class="transform opacity-100 scale-100"
-              leave-active-class="transition ease-in duration-75"
-              leave-from-class="transform opacity-100 scale-100"
-              leave-to-class="transform opacity-0 scale-95"
-            >
-              <MenuItems
-                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-              >
-                <MenuItem v-slot="{ active }">
-                  <a
-                    href="profile"
-                    :class="[
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700',
-                    ]"
-                    >Your Profile</a
-                  >
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a
-                    href="#"
-                    @click="logout()"
-                    :class="[
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700',
-                    ]"
-                    >Sign out</a
-                  >
-                </MenuItem>
-              </MenuItems>
-            </transition>
-          </Menu>
+          <!-- Website Logo -->
+          <div>
+            <a href="#" class="flex items-center py-4 px-2">
+              <img src="../assets/logo.png" alt="Logo" class="h-8 w-8 mr-2">
+              <span class="font-semibold text-gray-500 text-lg">Forum</span>
+            </a>
+          </div>
+
+          <!-- Primary Navbar items -->
+          <div class="hidden md:flex items-center space-x-1">
+            <router-link v-for="item in navigation" :key="item.name" :to="item.href" :class="[
+              item.isactive
+                ? 'bg-gray-900 text-white'
+                : 'text-black font-bold hover:bg-gray-700 hover:text-white',
+              'px-3 py-2 rounded-md text-sm font-medium',
+            ]">{{ item.name }}</router-link>
+          </div>
+        </div>
+
+        <div class=" flex items-center">
+          <!-- user menu -->
+          <Popper>
+            <MenuButton
+            class="flex mr-3 rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <span class="sr-only">Open user menu</span>
+            <img v-if="userState.user" class="h-8 w-8 rounded-full" :src="userState.user.imageurl" alt="" />
+          </MenuButton>
+            <template #content>
+              <div class=" bg-white shadow-lg rounded-md mt-3 flex-col py-3 px-7">
+                <div> <router-link class=" py-4 w-full text-left text-sm text-gray-700" to="/profile">Your Profile</router-link></div>
+                <div> <router-link class="py-4 w-full text-left text-sm text-gray-700" to="/profile">Settings</router-link></div>
+                <div> <router-link class=" py-4 w-full text-left text-sm text-gray-700" to="/profile">Help</router-link></div>
+                <div> <router-link  @click="logout()" class=" py-4 w-full text-left text-sm text-gray-700" to="/signin">Sign out</router-link></div>
+              </div>
+            </template>
+          </Popper>
+          
+
+          <!-- Mobile menu button -->
+          <button class="outline-none mobile-menu-button md:hidden" @click="menuTrigger">
+            <i class="fa fa-bars text-xl text-black hover:text-green-600" v-if="!menu"></i>
+            <i class="fa fa-times text-xl text-black hover:text-red-600" v-else></i>
+          </button>
         </div>
       </div>
     </div>
 
-    <DisclosurePanel class="sm:hidden">
-      <div class="space-y-1 px-2 pt-2 pb-3">
-        <DisclosureButton
-          v-for="item in navigation"
-          :key="item.name"
-          as="a"
-          :href="item.href"
-          :class="[
-            item.current
-              ? 'bg-gray-900 text-white'
-              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-            'block px-3 py-2 rounded-md text-base font-medium',
-          ]"
-          :aria-current="item.current ? 'page' : undefined"
-          >{{ item.name }}</DisclosureButton
-        >
-      </div>
-    </DisclosurePanel>
-  </Disclosure>
+    <!-- mobile menu -->
+    <div v-if="menu" class="mobile-menu md:hidden">
+      <router-link v-for="item in navigation" :key="item.name" :to="item.href" :class="[
+        item.isactive
+          ? 'bg-gray-900 text-white'
+          : 'block text-sm font-bold px-2 py-4 hover:bg-green-500 transition duration-300',
+        'px-3 py-2 rounded-md text-sm font-medium',
+      ]">{{ item.name }}</router-link>
+    </div>
+
+  </nav>
 </template>
 
 <script>
-export default {
-  data() {},
+import { mapGetters } from 'vuex';
+import { defineComponent } from "vue";
+  import Popper from "vue3-popper";
+
+export default defineComponent({
+  data() {
+    return {
+      navigation: [
+        { name: "Home", href: "/" },
+        { name: "Team", href: "/category" },
+        { name: "About", href: "/about" },
+        { name: "Calendar", href: "/signin" },
+      ],
+      menu: false,
+    }
+  },
   methods: {
     logout: async function () {
       this.$store.dispatch("logout");
       return this.$router.push("/signin");
     },
+    menuTrigger() {
+      this.menu = !this.menu
+    },
   },
   computed: mapGetters({
     userState: "getUserState",
   }),
-};
+  components: {
+      Popper,
+    },
+});
+
 </script>
 
-<script setup>
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-} from "@headlessui/vue";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
-import { mapGetters } from "vuex";
-
-const navigation = [
-  { name: "Home", href: "/"},
-  { name: "Team", href: "/category" },
-  { name: "About", href: "/about" },
-  { name: "Calendar", href: "/signin" },
-];
-</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Notifications from 'notiwind'
+import Popper from "vue3-popper";
 
 // fontawesome
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
@@ -10,3 +11,5 @@ import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import"./assets/tailwind.css";
 
 createApp(App).use(Notifications).use(store).use(router).mount('#app')
+// eslint-disable-next-line vue/multi-word-component-names
+App.component("Popper", Popper);
